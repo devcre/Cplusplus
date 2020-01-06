93dd6aa8935ad032a83366689282ef20ebd3ec53
devcre <tigertank707@gmail.com>
2020-01-06 11:03:25 +0900

    Add files via upload

---
+++ Day02/codes/temperature1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    double tempf = 48;
+    double tempc;
+    tempc = (tempf - 32) / 1.8;
+    
+    std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+    
+}
